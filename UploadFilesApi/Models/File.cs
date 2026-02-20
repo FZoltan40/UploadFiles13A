eea3fde89e6b26e251f7d75a9aa94de007b553b6
FZoltan40 <farkasz@kkszki.hu>
2026-02-20 09:32:03 +0100

    Interfaces are created.

---
+++ UploadFilesApi/Models/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UploadFilesApi.Models;
+
+public partial class File
+{
+    public int Id { get; set; }
+
+    public byte[] FileData { get; set; } = null!;
+
+    public string FilName { get; set; } = null!;
+
+    public string ContentType { get; set; } = null!;
+}
---
+++ UploadFilesApi/Services/InteFaces/IUploadFile.cs
@@ -0,0 +1,8 @@
+﻿namespace UploadFilesApi.Services.InteFaces
+{
+    public interface IUploadFile
+    {
+        Task<object> UpLoad(IFormFile formFile);
+        Task<object> DownLoad(int id);
+    }
+}
